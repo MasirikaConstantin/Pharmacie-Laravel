252905743242219a105469c00ab78fcb69bc95ad
MasirikaConstantin <elconstant73@gmail.com>
2024-07-29 17:52:12 +0100

    Correction des tableau pour le Vendeur

--- resources/views/vendeur.blade.php
+++ resources/views/vendeur.blade.php
@@ -125,14 +125,14 @@
                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                                     </svg>
                                 </div>
-                                <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+                                <input type="search" id="myInput" onkeyup="filterTable()" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
                                 <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
                             </div>
                           </form>
 
-                          <div class="border-b border-gray-100 py-16 dark:border-gray-700 md:py-10 lg:py-20">
+                          <div class=" border-gray-100 py-16 dark:border-gray-700 md:py-10 lg:py-20">
                             <div class=" relative overflow-x-auto shadow-md sm:rounded-lg items-start gap-8 md:gap-10 lg:flex 2xl:gap-28">
-                                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                <table id="myTable" class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                         <tr>
                                             <th scope="col" class="px-6 py-3">
@@ -161,11 +161,11 @@
                                     <tbody>
                                         @foreach ($produits as $p )
                                         
-                                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                            <tr id="tr" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                                             <td class="px-6 py-4" >{{$i++}}</td>
-                                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                                     {{Str::limit($p->nom,50)}}
-                                                </th>
+                                                </td>
                                                 <td class="px-6 py-4">
                                                     {{$p->prix}}
 
@@ -207,36 +207,7 @@
                       
                                   <hr class="border-gray-200 dark:border-gray-600" />
                       
-                                  <div>
-                                    <p class="mb-3 text-sm font-medium text-gray-900 dark:text-white">Trade on the go with <a href="#" title="" class="underline hover:no-underline">Flowbite App</a></p>
-                      
-                                    <div class="gap-4 space-y-4 sm:flex sm:space-y-0">
-                                      <a href="#" class="inline-flex w-full items-center justify-center rounded-lg bg-gray-800 px-4 py-2.5 text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-500 dark:focus:ring-gray-800 sm:w-auto">
-                                        <svg class="mr-3 h-7 w-7" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google-play" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                                          <path fill="currentColor" d="M325.3 234.3L104.6 13l280.8 161.2-60.1 60.1zM47 0C34 6.8 25.3 19.2 25.3 35.3v441.3c0 16.1 8.7 28.5 21.7 35.3l256.6-256L47 0zm425.2 225.6l-58.9-34.1-65.7 64.5 65.7 64.5 60.1-34.1c18-14.3 18-46.5-1.2-60.8zM104.6 499l280.8-161.2-60.1-60.1L104.6 499z"></path>
-                                        </svg>
-                                        <div class="text-left">
-                                          <div class="mb-1 text-xs">Download on the</div>
-                                          <div class="-mt-1 font-sans text-sm font-semibold">Google Play</div>
-                                        </div>
-                                      </a>
-                      
-                                      <a href="#" class="inline-flex w-full items-center justify-center rounded-lg bg-gray-800 px-4 py-2.5 text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-500 dark:focus:ring-gray-800 sm:w-auto">
-                                        <svg class="mr-3 h-7 w-7" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="apple" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-                                          <path
-                                            fill="currentColor"
-                                            d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"
-                                          ></path>
-                                        </svg>
-                                        <div class="text-left">
-                                          <div class="mb-1 text-xs">Download on the</div>
-                                          <div class="-mt-1 font-sans text-sm font-semibold">Mac App Store</div>
-                                        </div>
-                                      </a>
-                                    </div>
-                                  </div>
-                      
-                                  <hr class="border-gray-200 dark:border-gray-600" />
+                                  
                       
                                   <div class="flex space-x-4">
                                     <a href="#" class="text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
@@ -286,23 +257,7 @@
                             </div>
                           </div>
                       
-                          <div class="py-6 md:py-8">
-                            <div class="gap-4 space-y-5 xl:flex xl:items-center xl:justify-between xl:space-y-0">
-                              <a href="#" title="" class="block">
-                                <img class="block h-8 w-auto dark:hidden" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/logo-full.svg" alt="" />
-                                <img class="hidden h-8 w-auto dark:block" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/logo-full-dark.svg" alt="" />
-                              </a>
-                      
-                              <ul class="flex flex-wrap items-center gap-4 text-sm text-gray-900 dark:text-white xl:justify-center">
-                                <li><a href="#" title="" class="font-medium hover:underline"> Flowbite Express </a></li>
-                                <li><a href="#" title="" class="font-medium hover:underline"> Legal Notice </a></li>
-                                <li><a href="#" title="" class="font-medium hover:underline"> Product Listing Policy </a></li>
-                                <li><a href="#" title="" class="font-medium hover:underline"> Terms of Use </a></li>
-                              </ul>
-                      
-                              <p class="text-sm text-gray-500 dark:text-gray-400">© 2024 <a href="#" class="hover:underline">Flowbite</a>, Inc. All rights reserved.</p>
-                            </div>
-                          </div>
+                          
                         </div>
                       </section>
 
@@ -310,4 +265,54 @@
             </div>
         </div>
     </div>
+    <script>
+      
+              // Sélectionnez tous les éléments du tableau
+var elements = document.querySelectorAll("table #tr");
+
+// Parcourez tous les éléments
+for(var i = 0; i < elements.length; i++) {
+    // Si l'élément est supérieur à 3, cachez-le
+    if(i >= 15) {
+        elements[i].style.display = "none";
+    }
+}
+
+      // Sélectionnez tous les éléments du tableau
+      var elements = document.querySelectorAll("table #trs");
+
+// Parcourez tous les éléments
+for(var i = 0; i < elements.length; i++) {
+    // Si l'élément est supérieur à 3, cachez-le
+    if(i >= 3) {
+        elements[i].style.display = "none";
+    }
+}
+
+    function filterTable() {
+
+// Declare variables
+var input, filter, table, tr, td, i, txtValue;
+input = document.getElementById("myInput");
+filter = input.value.toUpperCase();
+table = document.getElementById("myTable");
+tr = table.getElementsByTagName("tr");
+
+// Loop through all table rows, and hide those who don't match the search query
+for (i = 0; i < tr.length; i++) {
+  td = tr[i].getElementsByTagName("td")[1];
+  
+  if (td) {
+    txtValue = td.textContent || td.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      tr[i].style.display = "";
+    } else {
+      tr[i].style.display = "none";
+    }
+  }
+}
+
+}
+
+    </script>
 </x-app-layout>
